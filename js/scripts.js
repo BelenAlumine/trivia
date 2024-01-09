f98c2133c98429a88a47d50d5a7533c88291d02a
Belén Aluminé Callegaris <belen.alumine@gmail.com>
2024-01-09 16:13:56 -0300

    home page and files

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+const rootDiv = document.querySelector('#root')
+
+
+/*function createHomePage() {*/
+    const container = document.createElement('div')
+    const homePageContainer = document.createElement('div')
+    const img = document.createElement('img')
+    const button = document.createElement('button')
+
+    //set class
+    container.classList.add('container')
+    homePageContainer.classList.add('home-page-container')
+    img.classList.add('img-home')
+    button.classList.add('start-button')
+
+    //add div content
+    img.src = "./img/quiz.png"
+    img.alt = 'image quiz'
+    button.innerText = "Let's go!"
+
+    //anidar
+    homePageContainer.appendChild(img)
+    homePageContainer.appendChild(button)
+    container.appendChild(homePageContainer)
+
+
+
+rootDiv.appendChild(container)
