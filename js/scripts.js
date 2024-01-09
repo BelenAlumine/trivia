a1da47dc2b2b543cd0ace1881d86b0cd06464c3a
Belén Aluminé Callegaris <belen.alumine@gmail.com>
2024-01-09 19:13:30 -0300

    openGame() and cleanPage() functions

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,11 @@ function createHomePage() {
     const img = document.createElement('img')
     const button = document.createElement('button')
 
+    button.addEventListener('click', () => openGame())
+
     //set class
     container.classList.add('container')
+    container.id = 'container'
     homePageContainer.classList.add('home-page-container')
     img.classList.add('img-home')
     button.classList.add('start-button')
@@ -22,6 +25,53 @@ function createHomePage() {
     container.appendChild(homePageContainer)
 
     rootDiv.appendChild(container)
+}
+
+function openGame() {
+    cleanPage()
+    
+    //creat question
+    const container = document.querySelector('#container')
+    const gameContainer = document.createElement('div')
+    const questionContainer = document.createElement('div')
+    const question = document.createElement('p')
+    const optionContainer = document.createElement('div')
+    const a = document.createElement('p')
+    const b = document.createElement('p')
+    const c = document.createElement('p')
+    const d = document.createElement('p')
+
+    //set class
+    gameContainer.classList.add('game-container')
+    questionContainer.classList.add('question-container')    
+    optionContainer.classList.add('option-container')
+
+    //elements content
+    question.innerText = "What color was the white horse of San Martín?"
+    a.innerText = "Black"
+    b.innerText = "White"
+    c.innerText = "Blue"
+    d.innerText = "San Mar4tín did not have a horse"
+
+    //add elements
+    gameContainer.appendChild(questionContainer)
+    gameContainer.appendChild(optionContainer)
+    questionContainer.appendChild(question)
+    optionContainer.appendChild(a)
+    optionContainer.appendChild(b)
+    optionContainer.appendChild(c)
+    optionContainer.appendChild(d)
+    container.appendChild(gameContainer)
+
+}
+
+function cleanPage() {
+   const container = document.querySelector('#container')
+   console.log(container)
+   while (container.firstChild) {
+    
+    container.removeChild(container.firstChild)
+   }
 }
 
 createHomePage()
